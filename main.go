1e775a564feb522ba1dc54b3d930fdd0ec952afc
sunil <sunilc@netapp.com>
2025-09-08 21:34:05 +0530

    updated welcome msg

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World from Go and Docker!")
+	fmt.Fprintf(w, "Hello, World from Go and Docker!!!")
 }
 
 func main() {
